6149e91b365af96a7cb0d40ddb48fccef679cca6
EmmanuelEkwebelem <93545665+EmmanuelEkwebelem@users.noreply.github.com>
2022-12-17 17:12:22 -0500

    Update

--- main.py
+++ main.py
@@ -10,13 +10,9 @@ load_dotenv()
 db = SQLAlchemy()
 app = Flask(__name__)
 
-
 mysql_username = os.getenv("MYSQL_USERNAME")
 mysql_password = os.getenv("MYSQL_PASSWORD")
 mysql_host = os.getenv("MYSQL_HOST")
-
-db = SQLAlchemy()
-app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://' + mysql_username + ':' + mysql_password + '@' + mysql_host + ':3306/patient_portal'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -25,7 +21,6 @@ db.init_app(app)
 
 class Users(db.Model):
     __tablename__ = 'production_accounts'
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
@@ -34,7 +29,6 @@ class Users(db.Model):
     mrn = db.Column(db.String(80), unique=True, nullable=False)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     last_login = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
-
     def __init__(self, username, password, email, account_type, mrn, date_created, last_login):
         self.username = username
         self.password = password
@@ -43,8 +37,6 @@ class Users(db.Model):
         self.mrn = mrn
         self.date_created = date_created
         self.last_login = last_login
-
-
     def to_json(self):
         return {
             'id': self.id,
@@ -59,7 +51,6 @@ class Users(db.Model):
 
 class Patients(db.Model):
     __tablename__ = 'production_patients'
-
     id = db.Column(db.Integer, primary_key=True)
     mrn = db.Column(db.String(255))
     first_name = db.Column(db.String(255))
@@ -71,7 +62,6 @@ class Patients(db.Model):
     race = db.Column(db.String(255))
     insurance_type = db.Column(db.String(255), nullable=True)
     contact_mobile = db.Column(db.String(255), nullable=True)
-
     def __init__(self, mrn, first_name, last_name, zip_code, gender, dob, race, insurance_type, contact_mobile):
         self.mrn = mrn
         self.first_name = first_name
@@ -82,7 +72,6 @@ class Patients(db.Model):
         self.race = race
         self.insurance_type = insurance_type
         self.contact_mobile = contact_mobile
-
     def to_json(self):
         return {
             'id': self.id,
@@ -99,15 +88,12 @@ class Patients(db.Model):
 
 class Conditions_patient(db.Model):
     __tablename__ = 'production_patient_conditions'
-
     id = db.Column(db.Integer, primary_key=True)
-    mrn = db.Column(db.String(255), db.ForeignKey('patients.mrn'))
-    icd10_code = db.Column(db.String(255), db.ForeignKey('conditions.icd10_code'))
-
+    mrn = db.Column(db.String(255), db.ForeignKey('production_patients.mrn'))
+    icd10_code = db.Column(db.String(255), db.ForeignKey('production_conditions.icd10_code'))
     def __init__(self, mrn, icd10_code):
         self.mrn = mrn
         self.icd10_code = icd10_code
-
     def to_json(self):
         return {
             'id': self.id,
@@ -135,15 +121,12 @@ class Conditions(db.Model):
 
 class Medications_patient(db.Model):
     __tablename__ = 'production_patient_medications'
-
     id = db.Column(db.Integer, primary_key=True)
-    mrn = db.Column(db.String(255), db.ForeignKey('patients.mrn'))
-    med_ndc = db.Column(db.String(255), db.ForeignKey('medications.med_ndc'))
-
+    mrn = db.Column(db.String(255), db.ForeignKey('production_patients.mrn'))
+    med_ndc = db.Column(db.String(255), db.ForeignKey('production_medications.med_ndc'))
     def __init__(self, mrn, med_ndc):
         self.mrn = mrn
         self.med_ndc = med_ndc
-
     def to_json(self):
         return {
             'id': self.id,
@@ -153,15 +136,12 @@ class Medications_patient(db.Model):
     
 class Medications(db.Model):
     __tablename__ = 'production_medications'
-
     id = db.Column(db.Integer, primary_key=True)
     med_ndc = db.Column(db.String(255))
     med_human_name = db.Column(db.String(255))
-
     def __init__(self, med_ndc, med_human_name):
         self.med_ndc = med_ndc
         self.med_human_name = med_human_name
-
     def to_json(self):
         return {
             'id': self.id,
@@ -169,13 +149,9 @@ class Medications(db.Model):
             'med_human_name': self.med_human_name
         }
 
-
-
-
 @app.route('/')
 def index():
     return render_template('landing.html')
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -203,10 +179,9 @@ def login():
             msg = 'Incorrect username / password !'
     return render_template('/login.html', msg = msg)
 
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    msg = 'Something Went Wrong'
+    msg = ''
     if request.method == 'POST' and 'account_type' in request.form:
         if request.form['account_type'] == 'admin':
             return redirect(url_for('register_admin'))
@@ -218,10 +193,9 @@ def register():
         msg = 'Please fill out the form!'
     return render_template('register.html', msg=msg)
 
-
 @app.route('/register/admin', methods=['GET', 'POST'])
 def register_admin():
-    msg = 'Something Went Wrong'
+    msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
         username = request.form['username']
         password = request.form['password']
@@ -242,10 +216,9 @@ def register_admin():
         msg = 'Please fill out the form!'
     return render_template('register_admin.html', msg=msg)
 
-
-@app.route('/register/careprovider', methods=['GET', 'POST'])
+@app.route('/register/provider', methods=['GET', 'POST'])
 def register_provider():
-    msg = 'Something Went Wrong'
+    msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
         username = request.form['username']
         password = request.form['password']
@@ -266,7 +239,6 @@ def register_provider():
         msg = 'Please fill out the form!'
     return render_template('register_provider.html', msg=msg)
 
-
 @app.route('/register/patient', methods=['GET', 'POST'])
 def register_patient():
 
@@ -285,7 +257,7 @@ def register_patient():
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        
+
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         zip_code = request.form['zip_code']
@@ -304,22 +276,24 @@ def register_patient():
         else:
             datecreated = datetime.datetime.now()
             lastlogin = datetime.datetime.now()
+            
             new_user = Users(username, password, email, account_type, mrn, datecreated, lastlogin)
             new_patient = Patients(mrn, first_name, last_name, zip_code, dob, gender, contact_mobile, contact_home)
+
             db.session.add(new_user)
             db.session.commit()
             db.session.add(new_patient)
             db.session.commit()
+
             for condition in pt_conditions:
                 new_patient_condition = Conditions_patient(mrn, condition)
                 db.session.add(new_patient_condition)
                 db.session.commit()
+
             msg = 'You have successfully registered a PATIENT account !'
     elif request.method == 'POST':
         msg = 'Please fill out the form!'
     return render_template('register_patient.html', msg=msg, conditions=db_conditions, medications=db_medications)
-
-
 
 @app.route('/account')
 def account():
@@ -328,7 +302,6 @@ def account():
         print('Account details: ', account.to_json())
         return render_template('account.html', account=account)
     return redirect(url_for('login'))
-
 
 @app.route('/logout')
 def logout():
@@ -349,7 +322,7 @@ def get_gui_patients():
         return redirect(url_for('get_patient_details', mrn=session['mrn']))
 
 @app.route('/insert', methods = ['POST'])
-def insert():
+def insert(): 
     if request.method == 'POST':
         mrn = request.form['mrn']
         first_name = request.form['first_name']
@@ -366,7 +339,7 @@ def insert():
         return redirect(url_for('get_gui_patients'))
 
 @app.route('/update', methods = ['GET', 'POST'])
-def update():
+def update(): 
     if request.method == 'POST':
         form_mrn = request.form.get('mrn')
         patient = Patients.query.filter_by(mrn=form_mrn).first()
@@ -378,7 +351,7 @@ def update():
         return redirect(url_for('get_gui_patients'))
 
 @app.route('/delete/<string:mrn>', methods = ['GET', 'POST'])
-def delete(mrn):
+def delete(mrn): 
     patient = Patients.query.filter_by(mrn=mrn).first()
     print('Found patient: ', patient)
     db.session.delete(patient)
@@ -400,7 +373,7 @@ def get_patient_details(mrn):
         db_conditions = db_conditions, db_medications = db_medications)
 
 @app.route('/update_conditions', methods = ['GET', 'POST'])
-def update_conditions():
+def update_conditions(): 
     if request.method == 'POST':
         form_id = request.form.get('id')
         print('form_id', form_id)
@@ -427,7 +400,7 @@ def add_condition():
         return redirect(url_for('get_patient_details', mrn=form_mrn))
 
 @app.route('/delete_condition', methods = ['GET', 'POST'])
-def delete_condition(): 
+def delete_condition():
     if request.method == 'POST':
         form_mrn = request.form.get('mrn')
         form_icd10_code = request.form.get('icd10_code')
@@ -442,7 +415,7 @@ def delete_condition():
         return redirect(url_for('get_patient_details', mrn=form_mrn))
 
 @app.route('/add_medication', methods = ['GET', 'POST'])
-def add_medication():
+def add_medication(): 
     if request.method == 'POST':
         form_mrn = request.form.get('mrn')
         print('form_mrn', form_mrn)
@@ -461,7 +434,6 @@ def get_patients():
         return jsonify([patient.to_json() for patient in patients])
     else:
         return jsonify({'error': 'Not logged in, please log in first'})
-
 
 @app.route("/api/patients/<string:mrn>", methods=["GET"])
 def get_patient(mrn):
@@ -482,7 +454,7 @@ def create_patient():
     db.session.add(patient)
     db.session.commit()
     return jsonify(patient.to_json()), 201
- 
+
 @app.route('/api/patient/<string:mrn>', methods=['PUT'])
 def update_patient(mrn):
     if not request.json:
